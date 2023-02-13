8ff1aae1349e489900c7deae5166f392fd6472fb
Xabier Naseem <naseemxabier@gmail.com>
2023-02-13 19:32:03 +0100

    done

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ require('dotenv').config();
 const express = require('express');
 const hbs = require('hbs');
 
-// require spotify-web-api-node package here:
 
+// require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
 const app = express();
 
 app.set('view engine', 'hbs');
@@ -13,6 +14,53 @@ app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
 
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  });
+  
+  // Retrieve an access token
+  spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error));
+
 // Our routes go here:
+app.get("/", (req, res, next) => {
+    res.render("home")
+})
+
+app.get("/artist-search", (req, res, next) => {
+
+    spotifyApi.searchArtists(req.query.name)
+    .then(data => {
+      console.log('The received data from the API: ', data.body.artists.items[0]);
+      res.render("artist-search-results", data.body.artists)
+      // ----> 'HERE'S WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+    })
+    .catch(err => console.log('The error while searching artists occurred: ', err));
+})
+app.get('/albums/:artistId', (req, res, next) => {
+    let artistId = req.params.artistId
+    spotifyApi.getArtistAlbums(artistId)
+    .then(artist => {
+        res.render("albums", artist.body);
+    })
+
+
+    .catch(err => next(err))
+  });
+
+app.get('/tracks/:trackId', (req, res, next) => {
+    let trackId = req.params.trackId
+    spotifyApi
+    .getAlbumTracks(trackId)
+    .then(track => {
+        
+        res.render("tracks", track.body)
+    })
+    .catch(err => next(err))
+   
+}) 
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
